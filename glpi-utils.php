7723e9d2f89a6a221d0c987e5378c30aaba27ab5
obb-collab <paktreg@gmail.com>
2025-09-05 10:57:20 +0300

    feat: add read replica support

--- glpi-utils.php
+++ glpi-utils.php
@@ -20,3 +20,41 @@ function gexe_get_current_glpi_uid() {
     }
     return 0;
 }
+
+/**
+ * Obtain connection to GLPI database.
+ *
+ * @param string $mode 'ro' for read-only replica, anything else for master
+ * @return wpdb
+ */
+function gexe_glpi_db($mode = 'rw') {
+    static $rw = null;
+    static $ro = null;
+
+    if ($mode === 'ro') {
+        if ($ro instanceof wpdb) {
+            return $ro;
+        }
+        // Local replica for read operations
+        $ro = new wpdb(
+            'wp_glpi',            // db user
+            'xapetVD4OWZqw8f',    // db password
+            'glpi',               // db name
+            '127.0.0.1'           // local replica host
+        );
+        return $ro;
+    }
+
+    if ($rw instanceof wpdb) {
+        return $rw;
+    }
+
+    // Master server for write operations
+    $rw = new wpdb(
+        'wp_glpi',            // db user
+        'xapetVD4OWZqw8f',    // db password
+        'glpi',               // db name
+        '192.168.100.12'      // master host
+    );
+    return $rw;
+}
